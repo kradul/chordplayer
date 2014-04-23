f3e8484a73b00a55cf4fd3bee4d9e4247f1cc869
kradul <katya.radul@gmail.com>
2014-04-22 23:19:16 -0400

    button plays note, yay

--- chordplayer.js
+++ chordplayer.js
@@ -1,3 +1,55 @@
-console.log("HIIIII");
+$( document ).ready(function() {
+	MIDI.loadPlugin({
+			soundfontUrl: "./midi/soundfont/",
+			instrument: "acoustic_grand_piano",
+			callback: function(){}
+		});
+
+	console.log("HIIIII");
+
+	$("#play").click(function () {
+		var delay = 0; // play one note every quarter second
+			var note = 50; // the MIDI note
+			var velocity = 127; // how hard the note hits
+			// play the note
+			MIDI.setVolume(0, 127);
+			MIDI.noteOn(0, note, velocity, delay);
+			MIDI.noteOff(0, note, delay + 0.75);
+	});
+});
 
 //start a local webserver python -m SimpleHTTPServer 8000
+
+
+
+	// teoria.note.getNoteMIDI  = function (note) {
+		
+	// 		var midiValue = (note.noteOctave + 1) * 12;
+		
+	// 		switch (note.noteId) {
+				
+	// 			case 0:
+	// 				midiValue += note.noteAccidental;
+	// 				break;
+	// 			case 1:
+	// 				midiValue += note.noteAccidental + 2;
+	// 				break;
+	// 			case 2:
+	// 				midiValue += note.noteAccidental + 4;
+	// 				break;
+	// 			case 3:
+	// 				midiValue += note.noteAccidental + 5;
+	// 				break;
+	// 			case 4:
+	// 				midiValue += note.noteAccidental + 7;
+	// 				break;
+	// 			case 5:
+	// 				midiValue += note.noteAccidental + 9;
+	// 				break;
+	// 			case 6:
+	// 				midiValue += note.noteAccidental + 11;
+	// 				break;
+	// 		}
+			
+	// 		return midiValue;	
+	// 	};
