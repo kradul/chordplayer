f14c7647698eef29abba24f373eb2c0c35b12e5a
kradul <katya.radul@gmail.com>
2014-04-26 18:28:58 -0400

    all basic functionality exists, chords are named as they are played

--- chordnamer.js
+++ chordnamer.js
@@ -1,6 +1,11 @@
 
 function ChordNamer () {
-	
+	var path = "./chordnames/";
+	new Audio('./Interlude2.mp3').play();
 
+	this.name_chord = function (degree) {
+		file = path + degree + ".mp3";
+		new Audio(file).play();
+	}
 
 }
--- chordplayer.js
+++ chordplayer.js
@@ -1,6 +1,13 @@
 
-$( document ).ready(function() {
-	MIDI.loadPlugin({
+function ChordPlayer () {
+	var self = this;
+	this.chordNamer = new ChordNamer();
+	this.progression = new Progression();
+	this.progression.scale = new Scale();
+	this.progression.tonic = midi_octave_notes[0]; //tonic = middle c
+
+	this.load = function () {
+		MIDI.loadPlugin({
 			soundfontUrl: "./midi/soundfont/",
 			instrument: "acoustic_grand_piano",
 			callback: function(){
@@ -8,43 +15,41 @@ $( document ).ready(function() {
 				$("#play").removeAttr("disabled");
 			}
 		});
+	};
 
-	var generate_chord = function (progression){
-		var degree = Math.floor((Math.random()*progression.scale.size())+1); //random number between 1 and scale size
-		var chord = progression.get_chord(degree);
+	this.play_chord = function (){
+		var degree = Math.floor((Math.random()*self.progression.scale.size())+1); //random number between 1 and scale size
+		var chord = self.progression.get_chord(degree);
 
 		MIDI.chordOn(0, chord, 127, 0);
+		self.chordNamer.name_chord(degree);
 		MIDI.chordOff(0, chord, 127, 1.75);
-	}
+	};
 
 	//for the timeout
 	var intervalId;
 
 	$("#play").click(function () {
-		new Audio('./Interlude2.mp3').play();
-
-		/*
-		var p = new Progression();
-		p.scale = new Scale();
-		p.tonic = midi_octave_notes[0]; //tonic = middle c
-
-
 		var delay = 0; // play one note every quarter second
-		var note = p.tonic; // the MIDI note
+		var note = self.progression.tonic; // the MIDI note
 		var velocity = 127; // how hard the note hits
 		// play the note
 		MIDI.setVolume(0, 127);
 		MIDI.noteOn(0, note, velocity, delay);
 		MIDI.noteOff(0, note, delay + 0.75);
 
-		intervalId = setInterval(function() { generate_chord(p); }, 1000);
-		*/
+		intervalId = setInterval(self.play_chord, 1000);
 	});
 
 	$("#pause").click(function () {
 		clearInterval(intervalId);
 	});
 
+}
+
+
+$( document ).ready(function() {
+	new ChordPlayer().load();
 });
 
 //start a local webserver python -m SimpleHTTPServer 8000
