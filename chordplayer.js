8433c043d61fdf5c6d1524c08d451f0f2c502e45
Katya Radul <kradul@mit.edu>
2014-05-01 19:35:56 -0400

    added a delay slider

--- chordplayer.js
+++ chordplayer.js
@@ -5,10 +5,19 @@ var MIN_PERIOD = 1000;
 var MAX_PERIOD = 3000;
 var PERIOD_INCR = 300;
 
+//various variables relating to delay (in milliseconds), time after which chord name is announced
+var DEFAULT_DELAY = 1000;
+var MIN_DELAY = 0; 
+//no max delay because the delay is not allwed to be longer than the period
+var DELAY_INCR = 300;
+var DELAY_POST_PERIOD = 600; /*approximately the amount of time it takes to name a chord, 
+buffer period so that a chord is not still being named when a new chord starts playing
+DON'T change this to be larger than the minimum period*/
+
 function ChordPlayer () {
 	var self = this;
 	this.period = DEFAULT_PERIOD; //time between chord play in milliseconds
-	this.delay = 1000; //time after chord that name is played
+	this.delay = DEFAULT_DELAY; //time after chord that name is played
 	this.chordNamer = new ChordNamer();
 	this.progression = new Progression();
 	this.progression.scale = new Scale();
@@ -39,7 +48,7 @@ function ChordPlayer () {
 		setTimeout(function() { self.chordNamer.name_chord(degree); }, self.delay);
 	};
 
-	
+
 	this.is_playing = function () {
 		return self.loaded & Boolean($("#play").attr("disabled"));
 	};
@@ -50,6 +59,19 @@ function ChordPlayer () {
 
 	this.disable_play = function () {
 		$("#play").attr("disabled", "disabled");
+	}
+
+	this.get_max_delay = function () {
+		//do not allow the delay to be longer than the period
+		return self.period - DELAY_POST_PERIOD; //enough time for the chord to be named
+	}
+
+	this.update_delay = function (period) {
+		/*update the delay slider and the actual delay based on the period*/ 
+		var max_delay = self.get_max_delay();
+		$("#delay_slider").slider('option', 'max', max_delay);
+		//do not allow the delay to be larger than the new max
+		self.delay = self.delay > max_delay ? max_delay : self.delay;
 	}
 
 	/*
@@ -83,12 +105,24 @@ function ChordPlayer () {
 
 	$("#period_slider").bind('slidechange', function(event, ui) {
 		self.period = ui.value;
+		self.update_delay(); 
 
 		//if currently playing, reset play_chord to be called on the new period 
 		if(self.is_playing()){
 			clearInterval(self.intervalId);
 			self.intervalId = setInterval(self.play_chord, self.period);
 		}
+	});
+
+	$("#delay_slider").slider({
+		min: MIN_DELAY,
+		max: self.get_max_delay(), 
+		step: DELAY_INCR,
+		value: self.delay
+	});
+
+	$("#delay_slider").bind('slidechange', function(event, ui) {
+		self.delay = ui.value;
 	});
 
 }
